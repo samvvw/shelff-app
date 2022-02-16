2d27fd64c3b9d40ea11ec3425a6a1e95d3799543
Alejandra Hernandez <alerika@hotmail.comhttps://www.linkedin.com/jobs/view/2886603553/?refId=f3ae957d-e770-4bca-ae5b-99ee27824b39>
2022-02-15 22:48:05 -0800

    fixed conflict with appStack

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
+import AppStack from "./src/stacks/AppStack";
 import { SSRProvider } from "@react-aria/ssr";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import SignUp from "./src/screens/SignUp";
-import Sign from "./src/screens/Sign";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SSRProvider>
       <NativeBaseProvider>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Sign"
-              component={Sign}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="SignUp"
-              component={SignUp}
-              options={{ headerShown: true }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <AppStack />
       </NativeBaseProvider>
     </SSRProvider>
   );
---
+++ src/stacks/AppStack.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnboardingScreen from "../screens/onboarding/OnboardingScreen";
+
+import Sign from "../screens/Sign";
+import SplashScreen from "../screenSplashScreen";
+import SignUp from "../screens/SignUp";
+
+const Stack = createNativeStackNavigator();
+
+const AppStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen
+          name="Sign"
+          component={Sign}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: true }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppStack;
