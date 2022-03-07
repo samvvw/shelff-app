aa904424b81f9432e80c9d9ee351691e043c45ba
Jose Arteaga <jh.arteaga09@gmail.com>
2022-03-07 12:53:12 -0800

    fix: change jsx file extensions to js

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -3,51 +3,51 @@ import { Center } from 'native-base'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const SplashScreen = ({ navigation }) => {
-    const checkFirstLaunch = async () => {
-        // AsyncStorage.clear() //to check onboarding
-        const firstTimeCheck = await AsyncStorage.getItem('isFirstTimeOpen')
-        console.log('firstTimeCheck', firstTimeCheck)
-        if (firstTimeCheck == null) {
-            return true
-        } else {
-            return false
-        }
+  const checkFirstLaunch = async () => {
+    // AsyncStorage.clear() //to check onboarding
+    const firstTimeCheck = await AsyncStorage.getItem('isFirstTimeOpen')
+    console.log('firstTimeCheck', firstTimeCheck)
+    if (firstTimeCheck == null) {
+      return true
+    } else {
+      return false
     }
+  }
 
-    useEffect(() => {
-        ;(async () => {
-            const isFirst = await checkFirstLaunch()
+  useEffect(() => {
+    ;(async () => {
+      const isFirst = await checkFirstLaunch()
 
-            if (isFirst) {
-                setTimeout(() => {
-                    navigation.replace('Onboarding')
-                }, 1000)
-            } else {
-                const token = await AsyncStorage.getItem('token')
-                if (token) {
-                    navigation.navigate('VerticalMenu')
-                } else {
-                    navigation.replace('GetStarted')
-                }
-            }
-        })()
-    }, [])
+      if (isFirst) {
+        setTimeout(() => {
+          navigation.replace('Onboarding')
+        }, 1000)
+      } else {
+        const token = await AsyncStorage.getItem('token')
+        if (token) {
+          navigation.navigate('VerticalMenu')
+        } else {
+          navigation.replace('GetStarted')
+        }
+      }
+    })()
+  }, [])
 
-    return (
-        <Center h="100%">
-            <Center
-                bg="primary.400"
-                _text={{
-                    color: 'white',
-                    fontSize: 40,
-                }}
-                h="30%"
-                w="50%"
-            >
-                Shelff
-            </Center>
-        </Center>
-    )
+  return (
+    <Center h="100%">
+      <Center
+        bg="primary.400"
+        _text={{
+          color: 'white',
+          fontSize: 40
+        }}
+        h="30%"
+        w="50%"
+      >
+        Shelff
+      </Center>
+    </Center>
+  )
 }
 
 export default SplashScreen
