7af505b2a34e0d306cc7eeb7bfcc192cb2a50f55
José Arteaga <jh.arteaga09@gmail.com>
2022-03-07 12:54:48 -0800

    Feature #50: add essentials items page (#52)
    * feat: add essentials items page
    
    - Add graphql query to get all essentials items by user id
    - Connect to server to get all essentials items
    
    * fix: change jsx file extensions to js

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,58 +3,63 @@ import {
   // Center,
   // Pressable,
   View,
-  Text,
+  Text
   // Icon,
   // MaterialIcons,
   // Box,
-} from "native-base";
-import React from "react";
+} from 'native-base'
+import React from 'react'
 
-import Category from "../components/list/Category";
-import Storage from "../components/list/Storage";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Category from '../components/list/Category'
+import Storage from '../components/list/Storage'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-import MyShelff from "../components/myShelff/MyShelff";
+import MyShelff from '../components/myShelff/MyShelff'
+import EssentialsScreen from '../screens/essentials/EssentialsScreen'
 
 function List({ navigation }) {
-  const Tab = createMaterialTopTabNavigator();
+  const Tab = createMaterialTopTabNavigator()
 
   return (
     <Tab.Navigator
       initialRouteName="Category"
       screenOptions={{
-        tabBarLabelStyle: { fontSize: 15, textTransform: "none" },
-        tabBarActiveTintColor: "#2c3e50",
-        tabBarIndicatorStyle: { backgroundColor: "#2c3e50" },
-        lazy: true,
+        tabBarLabelStyle: { fontSize: 15, textTransform: 'none' },
+        tabBarActiveTintColor: '#2c3e50',
+        tabBarIndicatorStyle: { backgroundColor: '#2c3e50' },
+        lazy: true
       }}
     >
-      <Tab.Screen name="Category" component={Category} navigation={navigation} />
+      <Tab.Screen
+        name="Category"
+        component={Category}
+        navigation={navigation}
+      />
       <Tab.Screen name="Storage" component={Storage} navigation={navigation} />
     </Tab.Navigator>
-  );
+  )
 }
 
 function Profile() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Profile!</Text>
     </View>
-  );
+  )
 }
 
 function Notifications() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Notifications!</Text>
     </View>
-  );
+  )
 }
 
 const Footer = () => {
-  const Tab = createBottomTabNavigator();
+  const Tab = createBottomTabNavigator()
   //tabBarActiveTintColor: "#e91e63",
   return (
     <Tab.Navigator
@@ -64,62 +69,62 @@ const Footer = () => {
           height: 70,
           paddingBottom: 20,
 
-          backgroundColor: "white",
+          backgroundColor: 'white'
         },
 
-        tabBarActiveTintColor: "red",
+        tabBarActiveTintColor: 'red'
       }}
     >
       <Tab.Screen
         name="MyShelff"
         component={MyShelff}
         options={{
-          tabBarLabel: "My Shelff",
+          tabBarLabel: 'My Shelff',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
+          )
         }}
       />
 
       <Tab.Screen
         name="Essentials"
-        component={Notifications}
+        component={EssentialsScreen}
         options={{
-          tabBarLabel: "Essentials",
+          tabBarLabel: 'Essentials',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="heart" color={color} size={size} />
-          ),
+          )
         }}
       />
       <Tab.Screen
         name="center"
         component={MyShelff}
         options={{
-          tabBarItemStyle: { height: 0 },
+          tabBarItemStyle: { height: 0 }
         }}
       />
       <Tab.Screen
         name="List"
         component={List}
         options={{
-          tabBarLabel: "List",
+          tabBarLabel: 'List',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="shape" color={color} size={size} />
-          ),
+          )
         }}
       />
       <Tab.Screen
         name="Profile"
         component={Profile}
         options={{
-          tabBarLabel: "Profile",
+          tabBarLabel: 'Profile',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
+          )
         }}
       />
     </Tab.Navigator>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,12 +1,22 @@
 import { gql } from '@apollo/client'
 
 export const ADD_USER = gql`
-    mutation AddUser($userId: String, $email: String, $fullName: String) {
-        addUser(userId: $userId, email: $email, fullName: $fullName) {
-            userId
-            email
-            fullName
-            creationDate
-        }
+  mutation AddUser($userId: String, $email: String, $fullName: String) {
+    addUser(userId: $userId, email: $email, fullName: $fullName) {
+      userId
+      email
+      fullName
+      creationDate
     }
+  }
+`
+
+export const GET_ESSENTIALS = gql`
+  query Essentials($userId: String) {
+    essentials(userId: $userId) {
+      itemId
+      itemName
+      creationDate
+    }
+  }
 `
