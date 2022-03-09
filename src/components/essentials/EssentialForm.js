15a20de733a294bf3a534692a8915a1e501edff0
José Arteaga <jh.arteaga09@gmail.com>
2022-03-08 18:57:50 -0800

    Feature/51: Create essential list and action buttons to add essentials to shelf (#68)
    * add modal when essential item is click added
    
    * create screen to add essential item to the shelf
    
    * add view screen to add new item from essentials item
    
    * feat: get items from local storage table for essentials per barcode
    
    Pending:
    - [ ] Connect with the backend server to bring essentials per barcode

---
+++ src/components/essentials/EssentialForm.js
@@ -0,0 +1,224 @@
+import { Switch } from 'native-base'
+import { useState } from 'react'
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import LocationList from '../elements/LocationList'
+import QuantityCounter from './QuantityCounter'
+import DateTimePicker from '@react-native-community/datetimepicker'
+import { saveItemsToLocalStorage } from '../barcode/saveItems'
+import uuid from 'react-native-uuid'
+
+const EssentialForm = ({ navigation, route }) => {
+    const todayDate = new Date()
+    const { item } = route.params
+    const [location, setLocation] = useState()
+    const [quantity, setQuantity] = useState(1)
+    const [isEssential, setIsEssential] = useState(true)
+    const [showCalendar, setShowCalendar] = useState(false)
+    const [today, setToday] = useState(
+        new Date(
+            todayDate.getFullYear(),
+            todayDate.getMonth(),
+            todayDate.getDate(),
+        ),
+    )
+
+    const [date, setDate] = useState('')
+    const [currentDate, setCurrentDate] = useState(today)
+
+    const onChange = (_, selectedDate) => {
+        setShowCalendar(false)
+        setDate(formatDate(selectedDate))
+
+        setCurrentDate(
+            new Date(
+                selectedDate.getFullYear(),
+                selectedDate.getMonth(),
+                selectedDate.getDate(),
+            ),
+        )
+    }
+
+    function formatDate(dateToFormat) {
+        return [
+            padTo2Digits(dateToFormat.getMonth() + 1),
+            padTo2Digits(dateToFormat.getDate()),
+            dateToFormat.getFullYear(),
+        ].join('/')
+    }
+
+    function padTo2Digits(num) {
+        return num.toString().padStart(2, '0')
+    }
+
+    const onSubmit = () => {
+        if (currentDate && location && quantity) {
+            const id = uuid.v4()
+            saveItemsToLocalStorage([
+                {
+                    idItem: id,
+                    cItemName: item.itemName,
+                    iQuantity: quantity,
+                    dexpirationdate: date,
+                    idCategory: item.categoryId,
+                    idLocation: location,
+                    essential: isEssential,
+                },
+            ])
+            navigation.goBack()
+        }
+    }
+
+    return (
+        <>
+            <View
+                style={{
+                    height: '100%',
+                    marginTop: 'auto',
+                }}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.itemCodeContainer}>
+                        <Text>{item?.barcode}</Text>
+                    </View>
+                    <View style={styles.itemNameContainer}>
+                        <Text style={styles.itemName}>{item?.itemName}</Text>
+                    </View>
+                    <View style={styles.row}>
+                        <View style={styles.pair}>
+                            <View style={styles.icon}></View>
+                            <Text>Dairy</Text>
+                        </View>
+                        <View style={styles.pair}>
+                            <TouchableOpacity
+                                onPress={() => setShowCalendar(true)}
+                            >
+                                <View style={styles.icon}></View>
+                            </TouchableOpacity>
+                            <Text>
+                                {date || `Expiration Date`}
+                                <Text style={{ color: '#f00' }}>*</Text>
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>
+                            Storage Location{' '}
+                            <Text style={{ color: '#f00' }}>*</Text>
+                        </Text>
+                        <LocationList
+                            location={location}
+                            setLocation={setLocation}
+                        />
+                    </View>
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>Quantity</Text>
+                        <QuantityCounter
+                            quantity={quantity}
+                            setQuantity={setQuantity}
+                        />
+                    </View>
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>
+                            Add to Essentials Items
+                        </Text>
+                        <Switch
+                            isChecked={isEssential}
+                            onToggle={() => setIsEssential((prev) => !prev)}
+                            size="lg"
+                        />
+                    </View>
+                    <View style={styles.buttonsContainer}>
+                        <TouchableOpacity onPress={() => onSubmit()}>
+                            <View style={[styles.button, styles.primary]}>
+                                <Text style={{ color: 'white' }}>
+                                    Add to my shelf
+                                </Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                            <View style={styles.button}>
+                                <Text>Cancel</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            {showCalendar && (
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={currentDate}
+                    mode={'date'}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                    minimumDate={today}
+                />
+            )}
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        borderRadius: 30,
+        backgroundColor: '#fff',
+        paddingHorizontal: 20,
+    },
+    itemCodeContainer: {
+        borderWidth: 1,
+        width: '50%',
+        alignItems: 'center',
+        borderColor: '#ccc',
+        alignSelf: 'center',
+        marginVertical: 30,
+        borderRadius: 5,
+    },
+    itemName: {
+        fontSize: 26,
+        fontWeight: 'bold',
+    },
+    button: {
+        alignItems: 'center',
+        marginVertical: 6,
+        padding: 15,
+        borderRadius: 8,
+        backgroundColor: '#e8e8e8',
+    },
+    primary: {
+        backgroundColor: '#307aff',
+        color: '#fff',
+    },
+    row: {
+        flexDirection: 'row',
+        marginVertical: 20,
+    },
+    pair: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 20,
+    },
+    icon: {
+        width: 30,
+        height: 30,
+        backgroundColor: '#ddda',
+        borderRadius: 5,
+        marginRight: 5,
+    },
+    formControl: {
+        flexDirection: 'row',
+        marginTop: 20,
+        justifyContent: 'space-between',
+        paddingBottom: 20,
+        borderBottomWidth: 2,
+        borderColor: '#eee',
+    },
+    label: {
+        alignSelf: 'center',
+    },
+    buttonsContainer: {
+        marginTop: 20,
+    },
+})
+
+export default EssentialForm
