9c8c33db19c66ce69ca228049a2b687901ed9951
Jose Arteaga <jh.arteaga09@gmail.com>
2022-03-09 00:09:35 -0800

    add: get category query in queries.js file

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,22 +1,31 @@
 import { gql } from '@apollo/client'
 
 export const ADD_USER = gql`
-  mutation AddUser($userId: String, $email: String, $fullName: String) {
-    addUser(userId: $userId, email: $email, fullName: $fullName) {
-      userId
-      email
-      fullName
-      creationDate
+    mutation AddUser($userId: String, $email: String, $fullName: String) {
+        addUser(userId: $userId, email: $email, fullName: $fullName) {
+            userId
+            email
+            fullName
+            creationDate
+        }
     }
-  }
 `
 
 export const GET_ESSENTIALS = gql`
-  query Essentials($userId: String) {
-    essentials(userId: $userId) {
-      itemId
-      itemName
-      creationDate
+    query Essentials($userId: String) {
+        essentials(userId: $userId) {
+            itemId
+            itemName
+            creationDate
+        }
     }
-  }
+`
+
+export const GET_CATEGORIES = gql`
+    {
+        categories {
+            categoryId
+            categoryName
+        }
+    }
 `
