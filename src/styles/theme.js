f1084f4029556a7cd84497e65f1afeaedb0e0e94
Alejandra Hernandez <alerika@hotmail.com>
2022-03-13 21:22:08 -0700

    added styles/theme.js with pallete colors

---
+++ src/styles/theme.js
@@ -0,0 +1,36 @@
+export const theme = {
+    primaryColour: {
+        crimson: rgba(237, 64, 116, 1),
+        violetRed: rgba(174, 20, 84, 1),
+        primaryGrdient:
+            'linear-gradient(360deg, #4C095C -7.72%, #E9205D 79.58%)',
+    },
+    secondaryColour: {
+        lilac: '#B59EBE',
+        chip: '#F3F1F4',
+        pastelCrimson: rgba(247, 222, 231, 1),
+    },
+    statusColour: {
+        limeGreen: '#51B447',
+        darkCyan: 'rgba(5, 168, 152, 1)',
+        freshGradient: 'linear-gradient(180deg, #51B446 2%, #00A79D 100%)',
+        gold: 'rgba(255, 200, 8, 1)',
+        orange: 'rgba(252, 153, 4, 1)',
+        expiringGradient:
+            'linear-gradient(0.86deg, #FC9904 1.48%, #FFC908 100%)',
+        orangeRed: 'rgba(235, 92, 1, 1)',
+        firebrickRed: 'rgba(188, 22, 25, 1)',
+        expiredGradient:
+            'linear-gradient(360deg, #B80F1A -7.72%, #E05209 79.58%)',
+    },
+    textColour: {
+        title: 'rgba(17, 8, 19, 1)',
+        body: 'rgba(49, 42, 51, 1)',
+        inactiveText: 'rgba(170, 162, 173, 1)',
+    },
+    components: {
+        gradientCard: ' linear-gradient(180deg, #FFFFFF 0%, #F1F8F7 100%)',
+        chip: 'rgba(243, 241, 244, 1)',
+        boundaryLines: 'rgba(208, 200, 210, 1)',
+    },
+}
