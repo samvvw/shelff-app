feb6f0e81f6cf2ba978b88572ff481bdeb6f177a
Jose Arteaga <jh.arteaga09@gmail.com>
2022-03-23 22:08:51 -0700

    add animation for fresh items in Chart.js

--- src/components/myShelff/Chart.js
+++ src/components/myShelff/Chart.js
@@ -1,48 +1,69 @@
-import React, {useState} from 'react'
-import {View, Text, Image, Platform} from 'react-native'
-import { singleChartStyles } from "../../styles/styles"
-import { theme } from "../../styles/theme"
+import React, { useState } from 'react'
+import { View, Text, Image, Platform } from 'react-native'
+import { singleChartStyles } from '../../styles/styles'
+import { theme } from '../../styles/theme'
 import { CircularProgressWithChild } from 'react-native-circular-progress-indicator'
+import LottieView from 'lottie-react-native'
 //using 'WithChild' as it accept any child, which I need texts and image here inside of chart
 
 const Fresh = (props) => {
-
     const { totalItems, numOfItems, status } = props
-
 
     return (
         <View style={singleChartStyles.container}>
-
             <View style={singleChartStyles.numOfItem}>
-                <Text>{status}: {numOfItems} item{numOfItems <= 1 ? '' : 's'}</Text>
+                <Text>
+                    {status}: {numOfItems} item{numOfItems <= 1 ? '' : 's'}
+                </Text>
             </View>
 
             <CircularProgressWithChild
                 value={numOfItems}
                 maxValue={totalItems}
                 radius={Platform.OS === 'ios' ? 150 : 140}
-                activeStrokeColor={status=='Fresh' ? theme.statusColour.darkCyan : status=='Expiring' ? theme.statusColour.orange : theme.statusColour.firebrickRed}
-                inActiveStrokeColor={status=='Fresh' ? theme.statusColour.darkCyan : status=='Expiring' ? theme.statusColour.orange : theme.statusColour.firebrickRed}
+                activeStrokeColor={
+                    status == 'Fresh'
+                        ? theme.statusColour.darkCyan
+                        : status == 'Expiring'
+                        ? theme.statusColour.orange
+                        : theme.statusColour.firebrickRed
+                }
+                inActiveStrokeColor={
+                    status == 'Fresh'
+                        ? theme.statusColour.darkCyan
+                        : status == 'Expiring'
+                        ? theme.statusColour.orange
+                        : theme.statusColour.firebrickRed
+                }
                 activeStrokeWidth={25}
                 inActiveStrokeWidth={25}
                 inActiveStrokeOpacity={0.2}
             >
                 <View style={singleChartStyles.innerWrapper}>
-                    <Image 
-                        source={require('../../../assets/icon.png')} 
-                        alt={'Fresh'}
-                        style={singleChartStyles.image}    
-                    />
+                    {status === 'Fresh' && (
+                        <LottieView
+                            source={require('../../assets/fresh-animation.json')}
+                            autoPlay
+                            loop={true}
+                            style={{
+                                width: 140,
+                                height: 140,
+                                backgroundColor: '#fff',
+                                justifyContent: 'center',
+                            }}
+                        />
+                    )}
+                    {status !== 'Fresh' && (
+                        <Image
+                            source={require('../../../assets/icon.png')}
+                            alt={'Fresh'}
+                            style={singleChartStyles.image}
+                        />
+                    )}
                 </View>
             </CircularProgressWithChild>
-    
         </View>
-
-
     )
 }
 
-
 export default Fresh
-
-
